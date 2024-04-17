89ccfd54d0ff176a7ec86eb40932b6a14eab2ba6
towy <48205489+towy@users.noreply.github.com>
2024-04-17 13:10:10 +0800

    Update install_appstore.sh

--- install/install_appstore.sh
+++ install/install_appstore.sh
@@ -42,7 +42,29 @@ fi
 # 官方应用商店
 appstore_dir="$install_path/resource/apps/local/appstore-official"
 
-# 判断是否仅克隆官方仓库
+# 克隆官方应用商店资源
+echo "克隆官方应用商店资源..."
+git clone -b dev --depth 1 https://github.com/1Panel-dev/appstore "$appstore_dir"
+if [ $? -ne 0 ]; then
+    echo "克隆失败，请检查您的互联网连接和URL：https://github.com/1Panel-dev/appstore"
+    exit 1
+fi
+
+# 复制官方应用
+echo "复制应用..."
+for app in "$appstore_dir/apps/"*; do
+    app_name=$(basename "$app")
+    if [ ! -d "$install_path/resource/apps/local/$app_name" ]; then
+        cp -rf "$app" "$install_path/resource/apps/local/"
+    else
+        echo "跳过已存在的应用: $app_name"
+    fi
+done
+
+# 清理官方应用商店
+rm -rf "$appstore_dir"
+
+# 判断是否克隆第三方仓库
 if [[ -z $official_only ]]; then
   # 第三方应用商店，使用 git 拉取更新
   appstore_local_dir="$install_path/resource/apps/local/appstore-localApps"
@@ -63,30 +85,17 @@ if [[ -z $official_only ]]; then
           exit 1
       fi
   fi
-fi
 
-echo "克隆官方应用商店资源..."
-git clone -b dev --depth 1 https://github.com/1Panel-dev/appstore "$appstore_dir"
-if [ $? -ne 0 ]; then
-    echo "克隆失败，请检查您的互联网连接和URL：https://github.com/1Panel-dev/appstore"
-    exit 1
-fi
-
-echo "复制应用..."
-for appstore_dir in "$appstore_dir" "$appstore_local_dir"; do
-  if [[ -d $appstore_dir ]]; then 
-    for app in "$appstore_dir/apps/"*; do
+  # 复制第三方应用
+  echo "复制第三方应用..."
+  for app in "$appstore_local_dir/apps/"*; do
       app_name=$(basename "$app")
       if [ ! -d "$install_path/resource/apps/local/$app_name" ]; then
           cp -rf "$app" "$install_path/resource/apps/local/"
       else
           echo "跳过已存在的应用: $app_name"
       fi
-    done
-  fi 
-done
-
-# 清理官方应用商店
-rm -rf "$appstore_dir"
+  done
+fi
 
 echo "完成。" 
